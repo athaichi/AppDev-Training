400b1723eab978fc5dbec6bc8b78d64fa47f5813
Lanea Rohan <lanearohan@gmail.com>
2018-09-28 16:59:15 -0500

    Initial Commit

---
+++ Toy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toy2
+//
+//  Created by Lanea Rohan on 9/28/18.
+//  Copyright © 2018 Lanea Rohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
